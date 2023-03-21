e279db3f05b0a342c8827ef551fdb78d6671639d
idoshim-bgu <93877702+idoshim-bgu@users.noreply.github.com>
2023-03-21 22:06:45 +0200

    task2

---
+++ submissions/assignment1/task2/memsize_test.c
@@ -0,0 +1,20 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+int main(int argc, char *argv[])
+{
+    int msize = memsize();
+    void* p;
+    printf("before allocation: %d\n",msize);
+
+    p = malloc(20000);
+    msize = memsize();
+    printf("after allocation: %d\n",msize);
+
+    free(p);
+    msize = memsize();
+    printf("after free: %d\n",msize);
+
+    exit(0);
+}
