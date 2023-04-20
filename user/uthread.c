fe0ea123b5c279174413bd42950c8f3c9f64415a
Ido Shimshi <idshimshi@gmail.com>
2023-04-20 14:39:09 +0000

    uthread create exit and startall

--- user/uthread.c
+++ user/uthread.c
@@ -6,6 +6,11 @@ struct uthread *curThread;
 
 int nexttid = 1;
 
+void thread_start(void (*start_func)()) {
+  start_func();
+  uthread_exit();
+}
+
 void
 uthreadinit(void)
 {
@@ -33,16 +38,36 @@ found:
 
     t->tid = nexttid++;
     t->priority = priority;
-    char* stack = (char*) malloc(STACK_SIZE);
-    if (stack == 0) {
-        return -1;  // or another error code
-    }
-    memcpy(t->ustack, stack, STACK_SIZE);
 
     t->context.sp = (uint64) t->ustack + STACK_SIZE; // set stack pointer to top of stack
-    t->context.ra = (uint64) start_func; // set return address to starting function
-    t->state = RUNNABLE;
+    t->context.ra = (uint64) thread_start; // set return address to starting function
 
-    
+    *((void (**)(void)) (t->context.sp - 8)) = start_func;
+
+    t->state = RUNNABLE;
     return 0;
+}
+// when creating a thread. i now want to free the used stack once the thread exits, how do i do that?
+void uthread_exit(){
+    struct uthread *myThread = uthread_self();
+    myThread->state = FREE;
+
+    uthread_yield();
+}
+
+int uthread_start_all(){
+    static int first = 1;
+    
+    if (first) {
+        struct uthread *t;
+        first = 0;
+        for(t = uthreadList; t < &uthreadList[MAX_UTHREADS]; t++) {
+            if(t->state == RUNNABLE) {
+                curThread = t;
+                uthread_yield();
+            }
+        }
+        return 0;
+    }
+    return -1;
 }
